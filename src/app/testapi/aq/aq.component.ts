5f32a3ec3512ec483abd667995a8c901b8268f4d
sokkimthanh1 <thanhsk1991@gmail.com>
2021-05-18 16:41:12 +0700

    adding logo card thong bao

--- src/app/testapi/aq/aq.component.ts
+++ src/app/testapi/aq/aq.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AnhQuan } from '../model/anhquan';
 import { LichthisvService } from '../services/lichthisv.service';
 import { flyInOut, toggleTrigger, visibility } from 'src/app/animations/reuse/app.animation';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-aq',
@@ -21,7 +23,30 @@ import { flyInOut, toggleTrigger, visibility } from 'src/app/animations/reuse/ap
 export class AqComponent implements OnInit {
   anhquans: AnhQuan[];
   state: string;
-  constructor(private lichthisvservice: LichthisvService) { this.state = 'hidden'; }
+  moduleOverview2: string;
+  moduleOverview3: string;
+  moduleOverview4: string;
+  constructor(
+    private lichthisvservice: LichthisvService,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer) {
+    this.state = 'hidden';
+    this.moduleOverview2 = 'http://localhost:3000/svg/LICHTHI.svg';
+    this.moduleOverview3 = 'http://localhost:3000/svg/DIEMTHI.svg';
+    this.moduleOverview4 = 'http://localhost:3000/svg/THOIKHOABIEU.svg';
+    this.matIconRegistry.addSvgIcon(
+      'LICHTHI',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(this.moduleOverview2)
+    );
+    this.matIconRegistry.addSvgIcon(
+      'DIEMTHI',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(this.moduleOverview3)
+    );
+    this.matIconRegistry.addSvgIcon(
+      'THOIKHOABIEU',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(this.moduleOverview4)
+    );
+  }
 
   ngOnInit(): void {
     this.lichthisvservice.getAnhQuans().subscribe(anhquans => { this.anhquans = anhquans; this.state = 'show'; });
