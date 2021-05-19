b8c800254d9a3b148ed193126a3b687156f828bc
sokkimthanh1 <thanhsk1991@gmail.com>
2021-05-19 12:53:14 +0700

    adding searchbox2

--- src/app/testapi/aq/search-msssv/search-msssv.component.ts
+++ src/app/testapi/aq/search-msssv/search-msssv.component.ts
@@ -22,16 +22,16 @@ export class SearchMsssvComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this.lichthisvs$ = this.searchTerms.pipe(
-    //   // wait 300ms after each keystroke before considering the term
-    //   debounceTime(300),
+    this.lichthisvs$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
 
-    //   // ignore new term if same as previous term
-    //   distinctUntilChanged(),
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
 
-    //   // switch to new search observable each time the term changes
-    //   switchMap((term: string) => this.lichthisvservice.searchLichThiSV(term)),
-    // );
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.lichthisvservice.searchLichThiSV(term.toUpperCase())),
+    );
   }
 
 }
