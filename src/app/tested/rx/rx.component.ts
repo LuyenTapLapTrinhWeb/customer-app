e93a2e5ddbba7d6cc9a0dafcd40fd855ac2d8abc
sokkimthanh1 <thanhsk1991@gmail.com>
2021-05-30 17:43:17 +0700

    adding transition card

--- src/app/tested/rx/rx.component.ts
+++ src/app/tested/rx/rx.component.ts
@@ -17,18 +17,18 @@ export class RxComponent implements OnInit, AfterViewInit {
     // When the user scrolls the page, execute myFunction
     // window.onscroll = () => { this.myFunction(); };
     // Get the navbar
-    this.navbar = document.getElementById('navbar');
-    this.sticky = this.navbar.offsetTop;
-    fromEvent(this.navbar, 'scroll').subscribe(event => { this.myFunction(); });
+    // this.navbar = document.getElementById('navbar');
+    // this.sticky = this.navbar.offsetTop;
+    // fromEvent(this.navbar, 'scroll').subscribe(event => { this.myFunction(); });
   }
   // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
   // tslint:disable-next-line:typedef
   myFunction() {
     // Get the offset position of the navbar
-    if (window.pageYOffset >= this.sticky) {
-      this.navbar.classList.add('sticky');
-    } else {
-      this.navbar.classList.remove('sticky');
-    }
+    // if (window.pageYOffset >= this.sticky) {
+    //   this.navbar.classList.add('sticky');
+    // } else {
+    //   this.navbar.classList.remove('sticky');
+    // }
   }
 }
