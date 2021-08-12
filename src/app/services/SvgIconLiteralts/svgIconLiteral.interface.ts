5d3acdd4a2fd07d53ae00e5321e85652e7c1e568
sokkimthanh1 <thanhsk1991@gmail.com>
2021-08-12 11:54:47 +0700

    update maticonsvg config interface

--- src/app/services/SvgIconLiteralts/svgIconLiteral.interface.ts
+++ src/app/services/SvgIconLiteralts/svgIconLiteral.interface.ts
@@ -1,6 +1,12 @@
+import { Observable } from 'rxjs';
+
 export interface ISvgIconLiteral {
     matIconCustomName: string;
     svgCustomVector: string;
     addMatIconSvgCustom?: (matIconSrc: ISvgIconLiteral) => void;
     addMultiMatIconSvgCustom?: (matIconSrc: ISvgIconLiteral[]) => void;
+    getMatIconSVGByName?: (matIconCustomName: string) => Observable<ISvgIconLiteral>;
+    getMatIconSVGByGroupName?: (GROUP_NAME: string[]) => Observable<ISvgIconLiteral[]>;
+    addMatIconSvgCustomObservable?: (matIconCustomName: string) => void;
+    addMultiMatIconSvgCustomObservable?: (matIconMultipleCustomName: string[]) => void;
 }
