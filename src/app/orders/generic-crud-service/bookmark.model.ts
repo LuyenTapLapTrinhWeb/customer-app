7ad86f9bef6b73023901ba7e0e8c2077d9aeb432
sokkimthanh1 <thanhsk1991@gmail.com>
2021-08-21 22:33:09 +0700

    Generic Crud Service

---
+++ src/app/orders/generic-crud-service/bookmark.model.ts
@@ -0,0 +1,5 @@
+export class Bookmark {
+    public id: number;
+    public url: string;
+    public description: string;
+}
---
+++ src/app/orders/generic-crud-service/bookmark.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Bookmark } from './bookmark.model';
+import { CrudService } from './crud.service';
+
+import { environment } from '../environments/environment';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class BookmarkService extends CrudService<Bookmark, number> {
+
+    constructor(protected _http: HttpClient) {
+        super(_http, `${environment.api.baseUrl}/bookmarks`);
+    }
+
+}
